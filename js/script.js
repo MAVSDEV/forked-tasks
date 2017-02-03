da0db10f0cd2b523c8b6835e7582bd3a8b576cfe
c5231481 <Uladzimir.Malko@sap.com>
2017-02-03 20:28:10 +0300

    performance optimization

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("pizza-image").src = "views/images/pizzeria-min_100x75.jpg";
-document.getElementById("project-mobile-image").src = "img/lh5_100x61.jpg";
-document.getElementById("project-webperf-image").src = "img/lh6_111x69.jpg";
-document.getElementById("project-2048-image").src = "img/unnamed_111x56.jpg";
-document.getElementsByClassName("jullphoto")[0].src = "img/julia-min_70x70.jpg";
-document.getElementById("cameron-image").src = "img/profilepic_70x70.jpg";
